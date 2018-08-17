e4ff62387a030a9c98ff1c1f8427f93749b0fb28
Ennykay7 <ksperkins.ksp@gmail.com>
2018-08-17 03:52:47 -0700

    working game logic, only needs guessed letters check and style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,44 @@
-var words = ["ford", "chevrolet", "buick", "jeep", "ram", "chrysler", "cadillac", "dodge", "tesla"];
-
-var wins = 0;
-var userGuess;
-var numGuesses = 7;
-var currentWord = words[Math.floor(Math.random() * (words.length))];
-var wordLength = currentWord.length;
-var wordSpace = "";
-
-console.log(wordLength);
-console.log(currentWord);
-
-for (wordLength; wordLength > 0; wordLength--) {
+    var words = ["ford", "chevrolet", "buick", "jeep", "ram", "chrysler", "cadillac", "dodge", "tesla"];
+    var wins = 0;
+    var userGuess;
+    var numGuesses = 7;
+    var currentWord = words[Math.floor(Math.random() * (words.length))];
+    var wordLength = currentWord.length;
     var space = "-";
-    wordSpace = wordSpace + space;
-}
-
-console.log(wordSpace);
-
-document.getElementById("currentWord").innerHTML = wordSpace;
+    var wordSpace = space.repeat(wordLength);
+    var userGuess = "";
+    
+    document.getElementById("currentWord").innerHTML = wordSpace;
 
 document.onkeyup = function (event) {
 
     userGuess = event.key.toLowerCase();
 
-    console.log(currentWord.search(userGuess));
-    console.log(userGuess);
-
-    if (currentWord === wordSpace) {
+    if (numGuesses === 0) {
+        alert("GAME OVER");
+    } else if (currentWord === wordSpace) {
 
         wins++;
-        document.getElementById("userWins").innerHTML(wins);
-        var currentWord = words[Math.floor(Math.random() * (words.length))];
-        var wordLength = currentWord.length;
-        var wordSpace = "";
-        for (wordLength; wordLength > 0; wordLength--) {
-            var space = "-";
-            wordSpace = wordSpace + space;
-        }
+        document.getElementById("userWins").innerHTML = wins;
+        numGuesses = 7;
+        document.getElementById("guessesLeft").innerHTML = numGuesses;
+        currentWord = words[Math.floor(Math.random() * (words.length))];
+        wordLength = currentWord.length;
+        wordSpace = space.repeat(wordLength);
         document.getElementById("currentWord").innerHTML = wordSpace;
 
     } else if (currentWord.includes(userGuess)) {
-        
+
+        var check1 = currentWord.indexOf(userGuess);
+        var check2 = currentWord.lastIndexOf(userGuess);
+        var replace = wordSpace.split("");
+        replace.splice(check1, 1, userGuess);
+        replace.splice(check2, 1, userGuess);
+        wordSpace = replace.join("");
+        document.getElementById("currentWord").innerHTML = wordSpace;
+
+    } else {
+        numGuesses--;
+        document.getElementById("guessesLeft").innerHTML = numGuesses;
     }
 }
